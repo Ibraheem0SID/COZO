d1e0348ede45bf8b9c72372ce3b6444b89c73bb3
Ibraheem SID <ibraheem88saadi88@gmail.com>
2024-03-15 17:28:19 +0300

    supabase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:cozo/src/core/supabase.dart';
 import 'package:cozo/src/features/splash/presentation/views/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: SupabaseInfo.url,
+    anonKey: SupabaseInfo.anonKey,
+  );
+
   runApp(const Cozo());
 }
 
---
+++ lib/src/core/supabase.dart
@@ -0,0 +1,5 @@
+class SupabaseInfo {
+  static const String url = 'https://pmnzsdlfuberiovymctm.supabase.co';
+  static const String anonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtbnpzZGxmdWJlcmlvdnltY3RtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA1MDEzMTcsImV4cCI6MjAyNjA3NzMxN30.qqx7FtNvvO5gDsX1SOOLrFROHlLuLg8Ouzn2tERIbLA';
+}
